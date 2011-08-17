5f0e8c3fa8694c0ecde245ded1058e5daa083fe8
Yoann Lopes <yoann.lopes@nokia.com>
2011-08-17 12:48:52 +0200

    Added Spanish translation.

---
+++ qml/trafikanten-es.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>FavoritesView</name>
+    <message>
+        <location filename="FavoritesView.qml" line="13"/>
+        <source>Favorites</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <location filename="FavoritesView.qml" line="60"/>
+        <source>No favorites</source>
+        <translation>Sin favoritos</translation>
+    </message>
+</context>
+<context>
+    <name>NearbyView</name>
+    <message>
+        <location filename="NearbyView.qml" line="16"/>
+        <source>Nearby</source>
+        <translation>Cercano</translation>
+    </message>
+</context>
+<context>
+    <name>RecentsView</name>
+    <message>
+        <location filename="RecentsView.qml" line="11"/>
+        <source>Recents</source>
+        <translation>Recientes</translation>
+    </message>
+    <message>
+        <location filename="RecentsView.qml" line="28"/>
+        <source>No recents</source>
+        <translation>Sin recientes</translation>
+    </message>
+</context>
+<context>
+    <name>SearchView</name>
+    <message>
+        <location filename="SearchView.qml" line="11"/>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <location filename="SearchView.qml" line="20"/>
+        <source>Stop</source>
+        <translation>Paradero</translation>
+    </message>
+</context>
+<context>
+    <name>StopDataDelegate</name>
+    <message>
+        <location filename="StopDataDelegate.qml" line="122"/>
+        <source>now</source>
+        <translation>aho.</translation>
+    </message>
+</context>
+<context>
+    <name>StopDeviationDialog</name>
+    <message>
+        <location filename="StopDeviationDialog.qml" line="84"/>
+        <source>Close</source>
+        <translation>Salir</translation>
+    </message>
+</context>
+<context>
+    <name>StopSearchBox</name>
+    <message>
+        <location filename="StopSearchBox.qml" line="35"/>
+        <source>No results</source>
+        <translation>Sin resultados</translation>
+    </message>
+</context>
+<context>
+    <name>StopSectionDelegate</name>
+    <message>
+        <location filename="StopSectionDelegate.qml" line="12"/>
+        <source>Platform</source>
+        <translation>Plataforma</translation>
+    </message>
+</context>
+<context>
+    <name>TimetableView</name>
+    <message>
+        <source>No data</source>
+        <translation type="obsolete">Aucune donnée disponible</translation>
+    </message>
+    <message>
+        <location filename="TimetableView.qml" line="99"/>
+        <source>No departures found</source>
+        <translation>No hay datos</translation>
+    </message>
+</context>
+</TS>
